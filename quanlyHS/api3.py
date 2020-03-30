f98874a0b666187a75cda74e896b73a6ba593f55
Phuong Thao <ntpthao.ftu@gmail.com>
2020-03-30 21:03:10 +0700

    update

--- quanlyHS/api3.py
+++ quanlyHS/api3.py
@@ -1,12 +1,9 @@
-from flask import Blueprint, render_template, url_for, redirect, flash, jsonify, request
+from flask import Blueprint, render_template, url_for, redirect, flash, request
 from quanlyHS.db import db
 from quanlyHS.models import Tables
 from quanlyHS.forms import Diemdanh
 from wtforms import ValidationError
-from flask_wtf import FlaskForm
-from wtforms_sqlalchemy.fields import QuerySelectField
 from sqlalchemy import and_
-import json
 
 HS = Tables.HocSinh
 DD = Tables.DiemDanh
@@ -18,23 +15,21 @@ bp = Blueprint('api3', __name__)
 @bp.route("/diemdanh", methods=['POST', 'GET'])
 def diemdanh():
     dd = Diemdanh()
-    # if dd.validate_on_submit():
-    #     result = db.session.query(DD, HS).join(HS).filter(and_(DD.MaGV == 'GV1', HS.MaLop == '6A1')).all()
-    #     # dict_ = {}
-    #     # for i in result:
-    #     #     dictdcd
-        
+    if request.method == 'GET' and len(request.args) == 0:
+        return render_template('diemdanh.html', title='Home', dd=dd)
 
-    #     return render_template('diemdanh.html', title='Home', dd=dd, result= result)
-    MaGV = request.args['MaGV']
-    MaLop = request.args['MaLop']
-    print(MaGV, MaLop, type(MaGV), type(MaLop))
-    return render_template('diemdanh.html', title='Home', dd=dd)
+    if request.method == ('GET' or 'POST') and len(request.args) != 0:
+        dict_ = dict(request.args)
+        result = db.session.query(DD, HS).join(HS).filter(and_(DD.MaGV == dict_['MaGV'], HS.MaLop == dict_['MaLop'])).all()
+        result = [i for i in result]
+        return render_template('diemdanh.html', title='Home', dd=dd, result=result)
 
 
 
-@bp.route("/index", methods=['POST', 'GET'])
-def index(id):
-    dd = Diemdanh()
+@bp.route("/hocsinh/update", methods=['GET', 'POST'])
+def update():
     
-    return render_template('diemdanh.html', title='Home', dd=dd)
+    return redirect(url_for('api3.diemdanh'))
+
+# tao form nhu diem danh cac cai can sua, submit ->> database
+# , dien vao viet code vao
